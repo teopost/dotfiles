05e94fef94b736e8899c4bae5afe6bb1d6810bf4
Stefano Teodorani <s.teodorani@gmail.com>
2014-05-05 21:46:38 +0200

    Added support for subdirectory

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,20 +7,13 @@
 function copyFiles() {
 
 	filelist=$(find . -maxdepth 1 ! -name "." ! -name ".git" ! -name "Brewfile" ! -name Caskfile ! -name "*.md" ! -name $(basename $0))
-    mydir=$(pwd)
-    # $(grep  -lib "# dotfile:" --exclude $(basename $0) *)
 
 	for f in ${filelist}
 	do
- 		echo "Processing: $mydir / $f"
+ 		echo "Processing: $f"
  		file=$(basename $f)
- 		# cp  ${mydir}/${file} ~/.${file}
- 		rsync  -av --no-perms ${mydir}/${file} ~/${file}
- 		# do something on $f
+ 		rsync  -q -av --no-perms ${f} ~
 	done
-
-#	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh"  --exclude "README.md"  -av --no-perms . ~
-#	source ~/.bash_profile
 }
 
 
@@ -29,7 +22,8 @@ if [ "$1" == "--overwrite" -o "$1" == "-o" ]; then
 elif [ "$1" == "--update" -o "$1" == "-u" ]; then
     git pull origin master
 else 
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo "This may overwrite existing files in your home directory."
+	read -p "Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		copyFiles
